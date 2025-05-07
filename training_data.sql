547c01fd6e2ea48ef331383548fcff473cd7181c
Larissa de Santi <desantilarissa@gmail.com>
2025-05-07 16:00:44 -0300

    Create training_data.sql

---
+++ training_data.sql
@@ -0,0 +1,28 @@
+WITH params AS (
+    SELECT
+    1 AS TRAIN, # define TRAIN variable to build an independent EVAL set
+    2 AS EVAL
+    ),
+
+  daynames AS
+    (SELECT ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'] AS daysofweek),
+
+  taxitrips AS (
+  SELECT
+    (tolls_amount + fare_amount) AS total_fare, # create label (what will be predicted)
+    daysofweek[ORDINAL(EXTRACT(DAYOFWEEK FROM pickup_datetime))] AS dayofweek,
+    EXTRACT(HOUR FROM pickup_datetime) AS hourofday,
+    pickup_longitude AS pickuplon,
+    pickup_latitude AS pickuplat,
+    dropoff_longitude AS dropofflon,
+    dropoff_latitude AS dropofflat,
+    passenger_count AS passengers
+  FROM
+    `nyc-tlc.yellow.trips`, daynames, params
+  WHERE
+    trip_distance > 0 AND fare_amount > 0
+    AND MOD(ABS(FARM_FINGERPRINT(CAST(pickup_datetime AS STRING))),1000) = params.TRAIN -- using hashing to assign trips to the training dataset (remainder 1)
+  )
+
+  SELECT *
+  FROM taxitrips
